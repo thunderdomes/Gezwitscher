5ac77057ea952fb74c580f2a82abaff4cd3bd6c1
Arie <arie@wego.com>
2013-04-21 22:12:07 +0700

    update sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `tb_LA` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id_twit` int(100) NOT NULL,
+  `from` text NOT NULL,
+  `profile_image_url` text NOT NULL,
+  `date_twit` text NOT NULL,
+  'img_url' text NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=176 ;
