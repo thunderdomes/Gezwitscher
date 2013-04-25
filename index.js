5ac22c56ade2b9cc670e6c2ee6a0d56d6e42ead0
lateph <lateph@gmail.com>
2013-04-25 17:42:12 +0700

    bug fix

--- index.js
+++ index.js
@@ -2,22 +2,24 @@
 var Twit = require('twit')
 
 var http = require('net');
+
 var app                 = require('http').createServer(handler),
-    io                  = require('socket.io').listen(app),
+    io                  = require('socket.io').listen(app,{ log: false }),
     fs                  = require('fs'),
     mysql               = require('mysql'),
     connectionsArray    = [];
+
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '010503',
+	password : 'root',
 	database:'node_test'
 }),
     POLLING_INTERVAL = 1000,
     pollingTimer;
 connection.connect(function(err) {
   // connected! (unless `err` is set)
-  console.log( err );
+  //console.log( err );
 });
 
 // creating the server ( localhost:8000 )
@@ -42,16 +44,31 @@ function a(){
 setInterval(a,1*100);
 BritAmaMusicBankLive
 */
-var stream = T.stream('statuses/filter', { track: '#Vespa3V #VespaWeekender' })
+
+var hash = ['#xxx','#sex'];
+var stream = T.stream('statuses/filter', { track: hash });
 
 stream.on('tweet', function (tweet) {
-	console.log(tweet);
+	//console.log(tweet);
 	var ts = Date.now() / 1000;
-	var post  = {id_twit: tweet.id, from:tweet.user.screen_name,date_twit: tweet.created_at,content:tweet.text, profile_image_url:tweet.user.profile_image_url};
-	var query = connection.query('INSERT INTO tb_cintamonyet SET ?', post, function(err, result) {
-  // Neat!
+	
+	var length = hash.length,
+	    element = null;
+	for (var i = 0; i < length; i++) {
+	  element = hash[i];
+	  if(tweet.text.indexOf(element) !== -1){
+	  	var post  = {id_twit: tweet.id, from:tweet.user.screen_name,date_twit: tweet.created_at,content:tweet.text, profile_image_url:tweet.user.profile_image_url,filter:element};
+		var query = connection.query('INSERT INTO tb_LA SET ?', post, function(err, result) {});
+	  }
+	}
+
+	//if(!connectionsArray.length){
+		connectionsArray.forEach(function( tmpSocket ){
+			tmpSocket.volatile.emit( 'notification' , true );
+		});
+	//}
+	
 });
-})
 // on server started we can load our client.html page
 function handler ( req, res ) {
 	fs.readFile( __dirname + '/client.html' , function ( err, data ) {
@@ -65,51 +82,44 @@ function handler ( req, res ) {
 	});
 }
 
-var pollingLoop = function () {
-
-	// Doing the database query
-	var query = connection.query('SELECT * FROM tb_cintamonyet ORDER BY id DESC LIMIT 0 , 2000'),
-	/*
-SELECT  id ,  id_twit ,  from ,  profile_image_url ,  date_twit ,  content 
-FROM  tb_LA
-WHERE content LIKE  Gue lagi di LA LIGHTS INDIE MOVIE #moviedayout, acara seru ngumpul, ngulik dan bikin film
-	*/
-		users = []; // this array will contain the result of our db query
-
-	// resuming the connection that is paused each loop
-	connection.resume();
-
-	// setting the query listeners
-	query
-	.on('error', function(err) {
-		// Handle error, and 'end' event will be emitted after this as well
-		//console.log( err );
-		updateSockets( err );
-
-	})
-	.on('result', function( user ) {
-		// it fills our array looping on each user row inside the db
-		users.push( user );
-	})
-	.on('end',function(){
-		// loop on itself only if there are sockets still connected
-		if(connectionsArray.length) {
-			pollingTimer = setTimeout( pollingLoop, POLLING_INTERVAL );
-			connection.pause();
-
-			updateSockets({users:users});
-		}
-	});
-
-};
 // creating a new websocket to keep the content updated without any AJAX request
 io.sockets.on( 'connection', function ( socket ) {
 
 	//console.log('Number of connections:' + connectionsArray.length);
 	// starting the loop only if at least there is one user connected
 	if (!connectionsArray.length) {
-		pollingLoop();
+	//	pollingLoop();
 	}
+
+	socket.on('all', function (user_id, fn) {
+		var query = connection.query('select * from (SELECT * FROM tb_LA WHERE id > \''+user_id+'\' ORDER BY id DESC LIMIT 0 , 100) a ORDER BY ID'),
+		users = []; // this array will contain the result of our db query
+	// resuming the connection that is paused each loop
+		connection.resume();
+		console.log( 'Load From : '+user_id);
+		// setting the query listeners
+		query
+		.on('error', function(err) {
+			// Handle error, and 'end' event will be emitted after this as well
+			//console.log( err );
+			updateSockets( err );
+
+		})
+		.on('result', function( user ) {
+			// it fills our array looping on each user row inside the db
+			users.push( user );
+		})
+		.on('end',function(){
+			// loop on itself only if there are sockets still connected
+			if(connectionsArray.length) {
+				//pollingTimer = setTimeout( pollingLoop, POLLING_INTERVAL );
+				connection.pause();
+				fn({users:users,hash:hash});
+			//	updateSockets();
+			}
+		});
+	    
+	});
 
 	socket.on('disconnect', function () {
 		var socketIndex = connectionsArray.indexOf( socket );
@@ -117,11 +127,11 @@ io.sockets.on( 'connection', function ( socket ) {
 		if (socketIndex >= 0) {
 			connectionsArray.splice( socketIndex, 1 );
 		}
+		console.log( 'A socket disconnected ! total curent socket : '+connectionsArray.length );
 	});
-
-	console.log( 'A new socket is connected!' );
+	
 	connectionsArray.push( socket );
-
+	console.log( 'A new socket is connected! total curent socket : '+connectionsArray.length );
 });
 
 var updateSockets = function ( data ) {
